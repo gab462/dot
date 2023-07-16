a883ad9103fb0b20e12070a0b3d59c402aab8774
gab462 <gustavoaraujob1+1@gmail.com>
2023-07-16 09:00:37 -0300

    Add font config to sway titlebars & CPU % in bar

--- sway/.config/sway/bar.sh
+++ sway/.config/sway/bar.sh
@@ -1,6 +1,9 @@
-#!/bin/sh
+#!/usr/bin/env bash
+
+CPU="00.00%"
 
 while true; do
-    echo "Vol:" `amixer sget Master | grep '[0-9]\+%' -o` "|" "Mem:" `free -h | awk '/Mem/{print $3}'` "|" `date`
-    sleep 1
+    echo "VOL:" `amixer sget Master | grep '[0-9]\+%' -o` "|" "CPU:" $CPU "|" "MEM:" `free -h | awk '/Mem/{print $3}'` "|" `date`
+
+    CPU=$(awk '{u=$2+$4; t=$2+$4+$5; if (NR==1){u1=u; t1=t;} else print ($2+$4-u1) * 100 / (t-t1) "%"; }' <(grep 'cpu ' /proc/stat) <(sleep 1; grep 'cpu ' /proc/stat) | xargs printf "%05.2f%%")
 done

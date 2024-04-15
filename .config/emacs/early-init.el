85214f4257143f355ee48b6a474ed167902a1429
gab462 <gustavoaraujob1+1@gmail.com>
2024-04-15 18:57:26 -0300

    Use emacs reverse-video

--- .config/emacs/early-init.el
+++ .config/emacs/early-init.el
@@ -5,8 +5,8 @@
 ;    (convert-standard-filename
 ;      (expand-file-name  "var/eln-cache/" user-emacs-directory))))
 
-(load-theme 'wombat t)
 (set-face-attribute 'mode-line nil :box nil)
 ;(set-frame-parameter nil 'alpha-background 90)
 
 (add-to-list 'default-frame-alist '(font . "Monospace-18"))
+(add-to-list 'default-frame-alist '(reverse . t))

4ee5f728c5ec40c132ec652d030175d14c34ec9b
gab462 <gustavoaraujob1+1@gmail.com>
2024-08-12 14:07:36 -0300

    Tweak editor styling

--- .config/emacs/early-init.el
+++ .config/emacs/early-init.el
@@ -8,8 +8,8 @@
 (set-face-attribute 'mode-line nil :box nil)
 ;(set-frame-parameter nil 'alpha-background 90)
 
-(add-to-list 'default-frame-alist '(font . "Monospace-18"))
-(add-to-list 'default-frame-alist '(reverse . t))
+(add-to-list 'default-frame-alist '(font . "Gohu Font 14 Nerd Font-14"))
+;; (add-to-list 'default-frame-alist '(reverse . t))
 
 (tool-bar-mode -1)
 (scroll-bar-mode -1)

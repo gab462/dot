04dc3255cabc91343ab75878d90f4a04f5977b73
gab462 <62474485+gab462@users.noreply.github.com>
2024-11-20 12:15:04 -0300

    Clean up bar script

--- sway/.config/sway/bar.sh
+++ sway/.config/sway/bar.sh
@@ -1,9 +1,15 @@
 #!/usr/bin/env bash
 
 CPU="00.00%"
+VOL="00%"
+MEM="000Mi"
+DATE=$(date)
 
 while true; do
-    echo "VOL:" `amixer sget Master | grep '[0-9]\+%' -o` "|" "CPU:" $CPU "|" "MEM:" `free -h | awk '/Mem/{print $3}'` "|" `date`
+    echo "VOL:" $VOL "|" "CPU:" $CPU "|" "MEM:" $MEM "|" $DATE
 
-    CPU=$(awk '{u=$2+$4; t=$2+$4+$5; if (NR==1){u1=u; t1=t;} else print ($2+$4-u1) * 100 / (t-t1) "%"; }' <(grep 'cpu ' /proc/stat) <(sleep 1; grep 'cpu ' /proc/stat) | xargs printf "%05.2f%%")
+    CPU=$(awk '{u=$2+$4; t=$2+$4+$5; if (NR==1){u1=u; t1=t;} else print ($2+$4-u1) * 100 / (t-t1); }' <(grep 'cpu ' /proc/stat) <(sleep 1; grep 'cpu ' /proc/stat) | xargs printf "%05.2f%%")
+    VOL=$(amixer sget Master | grep '[0-9]\+%' -o)
+    MEM=$(free -h | awk '/Mem/{print $3}')
+    DATE=$(date)
 done

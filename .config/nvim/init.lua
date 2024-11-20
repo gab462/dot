205e304f97731c70acd0959716b0dd69f012ea93
gab462 <62474485+gab462@users.noreply.github.com>
2024-11-20 12:15:05 -0300

    Configure neovim's completeopt

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -9,6 +9,7 @@ vim.opt.incsearch = true
 vim.opt.guicursor = ""
 vim.opt.laststatus = 3
 vim.opt.winbar = "%=%f %m"
+vim.opt.completeopt = "menuone,popup"
 
 vim.opt.backup = false
 vim.opt.swapfile = false

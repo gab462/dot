8bdc0255156895a5196f2f1b18d3599dad217aaf
gab462 <62474485+gab462@users.noreply.github.com>
2024-11-20 12:15:05 -0300

    Change to arch machine

---
+++ home/me/nvim/init.lua
@@ -0,0 +1,9 @@
+vim.opt.mouse = ""
+vim.opt.guicursor = ""
+vim.opt.hlsearch = false
+vim.opt.laststatus = 3
+vim.opt.winbar = "%=%m %f"
+
+vim.opt.expandtab = true
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4

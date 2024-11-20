d19591434551c5f8ce417a5d02301be9152e4a86
gab462 <62474485+gab462@users.noreply.github.com>
2024-11-20 12:15:04 -0300

    Add emacs package & config

---
+++ emacs/early-init.el
@@ -0,0 +1,19 @@
+(tool-bar-mode -1)
+(scroll-bar-mode -1)
+(menu-bar-mode -1)
+
+(add-to-list 'default-frame-alist '(font . "iMWritingQuat Nerd Font-18"))
+(add-to-list 'default-frame-alist '(alpha-background . 90))
+(add-to-list 'default-frame-alist '(reverse . t))
+(set-face-attribute 'mode-line nil :box nil)
+
+(setq package-enable-at-startup nil)
+
+(when (fboundp 'startup-redirect-eln-cache)
+  (startup-redirect-eln-cache
+   (convert-standard-filename
+    (expand-file-name  "var/eln-cache/" user-emacs-directory))))
+
+(setq gc-cons-threshold (* 50 1000 1000))
+
+(setq initial-major-mode 'fundamental-mode)

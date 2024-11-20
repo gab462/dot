bcb804955756a254331e9af63bff75536521d809
gab462 <62474485+gab462@users.noreply.github.com>
2024-11-20 17:05:02 -0300

    Configure neovim colors

--- home/me/.config/nvim/init.lua
+++ home/me/.config/nvim/init.lua
@@ -7,3 +7,19 @@ vim.opt.winbar = "%=%m %f"
 vim.opt.expandtab = true
 vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4
+
+vim.api.nvim_create_autocmd("Colorscheme", {
+    callback = function()
+        for _, name in ipairs({ "Normal", "Winbar", "WinbarNC", "WinSeparator" }) do
+            vim.api.nvim_set_hl(0, name, { bg = nil })
+        end
+
+        for name, color in pairs({ ["Comment"] = "lightgreen",
+                                   ["PreProc"] = "lightred",
+                                   ["Constant"] = "lightblue" }) do
+            vim.api.nvim_set_hl(0, name, { fg = color })
+        end
+    end
+})
+
+vim.cmd.colorscheme("quiet")

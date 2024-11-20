081cb3bda1bdb04cac2ec5f4fbd508f325c029ec
gab462 <62474485+gab462@users.noreply.github.com>
2024-11-20 12:15:04 -0300

    From Debian Sid to Fedora Silverblue

--- install.sh
+++ install.sh
@@ -1,3 +1,29 @@
 #!/bin/sh
 
-stow -t $HOME --no-folding .
+stow -t ~/.var/app --no-folding .
+
+# $ gsettings set org.gnome.desktop.input-sources xkb-options "['ctrl:nocaps']"
+# $ flatpak list --columns=application --app
+# app.drey.Dialect
+# com.bitwarden.desktop
+# com.discordapp.Discord
+# com.github.Matoking.protontricks
+# com.github.libresprite.LibreSprite
+# com.github.tchx84.Flatseal
+# com.github.tenderowl.frog
+# com.leinardi.gst
+# com.obsproject.Studio
+# com.raggesilver.BlackBox
+# com.usebottles.bottles
+# com.valvesoftware.Steam
+# dev.alextren.Spot
+# hu.kramo.Cartridges
+# io.gitlab.news_flash.NewsFlash
+# io.neovim.nvim
+# net.davidotek.pupgui2
+# org.godotengine.Godot
+# org.mozilla.firefox
+# org.nickvision.tubeconverter
+# org.rncbc.qpwgraph
+# org.videolan.VLC
+# sh.ppy.osu

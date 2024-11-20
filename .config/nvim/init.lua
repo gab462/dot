5eae7dcbfd1829fe2d758a8f06aea6b77719750a
gab462 <62474485+gab462@users.noreply.github.com>
2024-11-20 12:15:05 -0300

    Add clojure support

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -12,14 +12,14 @@ vim.opt.backup = false
 vim.opt.swapfile = false
 vim.opt.mouse = ""
 
-vim.opt.termguicolors = false
+-- vim.opt.termguicolors = false
+--
+-- vim.api.nvim_create_autocmd("Colorscheme", { callback = function()
+--     vim.api.nvim_set_hl(0, "Normal", { ctermbg = "none" })
+--
+--     for k, v in pairs({ Comment = "green", Constant = "blue" }) do
+--         vim.api.nvim_set_hl(0, k, { ctermfg = v })
+--     end
+-- end})
 
-vim.api.nvim_create_autocmd("Colorscheme", { callback = function()
-    vim.api.nvim_set_hl(0, "Normal", { ctermbg = "none" })
-
-    for k, v in pairs({ Comment = "green", Constant = "blue" }) do
-        vim.api.nvim_set_hl(0, k, { ctermfg = v })
-    end
-end})
-
-vim.cmd.colorscheme("quiet")
+vim.cmd.colorscheme("retrobox")

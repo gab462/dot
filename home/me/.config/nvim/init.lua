e083edc33b651fc142f79f0aa4f64b4b49d8d332
gab462 <62474485+gab462@users.noreply.github.com>
2025-01-04 20:57:19 -0300

    Minor changes

--- home/me/.config/nvim/init.lua
+++ home/me/.config/nvim/init.lua
@@ -8,18 +8,5 @@ vim.opt.expandtab = true
 vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4
 
-vim.api.nvim_create_autocmd("Colorscheme", {
-    callback = function()
-        for _, name in ipairs({ "Normal", "Winbar", "WinbarNC", "WinSeparator" }) do
-            vim.api.nvim_set_hl(0, name, { bg = nil })
-        end
-
-        for name, color in pairs({ ["Comment"] = "lightgreen",
-                                   ["PreProc"] = "lightred",
-                                   ["Constant"] = "lightblue" }) do
-            vim.api.nvim_set_hl(0, name, { fg = color })
-        end
-    end
-})
-
-vim.cmd.colorscheme("quiet")
+vim.opt.termguicolors = false
+vim.cmd.colorscheme("vim")

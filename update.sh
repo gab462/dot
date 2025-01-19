c718a2966bb731775a36c2a6eed9fa57fae043fa
gab462 <62474485+gab462@users.noreply.github.com>
2025-01-19 18:29:39 -0300

    Add update script

---
+++ update.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+FILES=$(find . -type f \
+             -not -path "./.git/*" \
+             -not -path "./NOTES" \
+             -not -path "./PKGS" \
+             -not -path "./update.sh")
+
+for file in $FILES; do
+    if ! diff $file ${file:1} 1>/dev/null; then
+        echo Updating $file with ${file:1}
+        echo Diff:
+        diff $file ${file:1}
+        echo
+    fi
+done
+
+pacman -Qe | sed -e 's/ .*//' > PKGS
